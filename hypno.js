8729f28a4baa666c24810890abcda402a9880ed8
Kawandeep Virdee <whichlight@gmail.com>
2014-01-28 22:11:46 +0100

    added background fun

--- hypno.js
+++ hypno.js
@@ -1,4 +1,6 @@
-var videoInput = document.getElementById('video');
+var videoInput = document.createElement('video');
+videoInput.height=480;
+videoInput.width=640;
 var canvasInput = document.getElementById('canvas');
 var cc = canvasInput.getContext('2d');
 var width = canvasInput.width;
@@ -10,7 +12,7 @@ window.URL = window.URL || window.webkitURL || window.msURL || window.mozURL;
 // check for camerasupport
 if (navigator.getUserMedia) {
   // set up stream
-  
+
   // chrome 19 shim
   var videoSelector = {video : true};
   if (window.navigator.appVersion.match(/Chrome\/(.*?) /)) {
@@ -19,7 +21,7 @@ if (navigator.getUserMedia) {
       videoSelector = "video";
     }
   };
-  
+
   navigator.getUserMedia(videoSelector, function( stream ) {
     if (videoInput.mozCaptureStream) {
       videoInput.mozSrcObject = stream;
@@ -44,15 +46,65 @@ ctracker.start(videoInput);
 cc.fillStyle = "rgba(0,0,0,0.1)";
 
 
+var clippingCircle = function(){
+  var x = canvas.width / 2;
+  var y = canvas.height / 2;
+  var radius = canvas.height/2.7;
+  var startAngle = 0;
+  var endAngle = 2 * Math.PI;
+  var counterClockwise = false;
+  cc.beginPath();
+  cc.arc(x, y, radius, startAngle, endAngle, counterClockwise);
+  cc.lineWidth = 50;
+  cc.clip();
+}
+
+var backgroundPattern = function(){
+  //circle
+  var x = canvas.width / 2;
+  var y = canvas.height / 2;
+  var radius = canvas.height/2.7 + 10;
+  var startAngle = 0;
+  var endAngle = 2 * Math.PI;
+  var counterClockwise = false;
+  cc.beginPath();
+  cc.arc(x, y, radius, startAngle, endAngle, counterClockwise);
+  cc.lineWidth = 20;
+  cc.strokeStyle = "white";
+  cc.stroke();
+
+  //draw rays
+  var increment = Math.PI*2 / 80;
+  var length = canvas.width;
+  var x = canvas.width / 2;
+  var y = canvas.height / 2;
+
+  for(var angle = 0; angle < Math.PI*2; angle+=increment){
+    cc.beginPath();
+    cc.moveTo(x,y);
+    cc.lineTo(x+length*Math.cos(angle), y+length*Math.sin(angle));
+    cc.strokeStyle = 'white';
+    cc.lineWidth = 15;
+    cc.closePath();
+    cc.stroke();
+  }
+}
+
 function drawLoop() {
+
   requestAnimationFrame(drawLoop);
+
+  backgroundPattern();
+
+  clippingCircle();
+  cc.drawImage(videoInput, 0, 0, canvas.width, canvas.height);
   // cc.clearRect(0, 0, canvasInput.width, canvasInput.height);
   // Fade out
   cc.fillStyle = "rgba(0,0,0,0.1)";
   cc.fillRect(0, 0, width, height);
+  cc.lineWidth = 5;
   ctracker.draw(canvasInput);
-
-
-
 }
 drawLoop();
+
+
